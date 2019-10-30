b2223f5695d78233b210ac792919276d2c06c27b
kobideveleap <kobiedeveleap@gmail.com>
2019-10-30 15:06:57 +0200

    add tag script

--- CI/tag.bash
+++ CI/tag.bash
@@ -1,10 +1,10 @@
 #!/bin/bash
 if [[ ${GIT_BRANCH} == *"master"* ]]; then                    
-    docker tag echoapp:latest echoapp:1.0."${BUILD_NUMBER}"
+    docker tag echoapp:latest gcr.io/first-k8s-project-256708/echoapp:1.0."${BUILD_NUMBER}"
 fi
 if [[ ${GIT_BRANCH} == *"dev"* ]]; then
-    docker tag echoapp:latest echoapp:dev-"${GIT_COMMIT}"
+    docker tag echoapp:latest gcr.io/first-k8s-project-256708/echoapp:dev-"${GIT_COMMIT}"
 fi
 if [[ ${GIT_BRANCH} == *"staging"* ]]; then
-    docker tag echoapp:latest echoapp:staging-"${GIT_COMMIT}"
+    docker tag echoapp:latest gcr.io/first-k8s-project-256708/echoapp:staging-"${GIT_COMMIT}"
 fi

25ec109519c279f1bd1cec2d90a5f280b2c95f3e
kobideveleap <kobiedeveleap@gmail.com>
2019-10-30 16:28:24 +0200

    add tag script

--- CI/publish.bash
+++ CI/publish.bash
@@ -1,10 +1,10 @@
 #!/bin/bash
 if [[ ${GIT_BRANCH} == *"master"* ]]; then                    
-    gcloud docker push gcr.io/first-k8s-project-256708/echoapp:1.0."${BUILD_NUMBER}"
+    gcloud docker -- push eu.gcr.io/first-k8s-project-256708/echoapp:1.0."${BUILD_NUMBER}"
 fi
 if [[ ${GIT_BRANCH} == *"dev"* ]]; then
-    gcloud docker push gcr.io/first-k8s-project-256708/echoapp:dev-"${GIT_COMMIT}"
+    gcloud docker -- push eu.gcr.io/first-k8s-project-256708/echoapp:dev-"${GIT_COMMIT}"
 fi
 if [[ ${GIT_BRANCH} == *"staging"* ]]; then
-    gcloud docker push gcr.io/first-k8s-project-256708/echoapp:staging-"${GIT_COMMIT}"
+    gcloud docker -- push eu.gcr.io/first-k8s-project-256708/echoapp:staging-"${GIT_COMMIT}"
 fi

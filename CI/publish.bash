318c3ee44ad7eb6f0f4a24c18814e980258c9211
kobideveleap <kobiedeveleap@gmail.com>
2019-10-30 16:31:54 +0200

    add tag script

--- CI/publish.bash
+++ CI/publish.bash
@@ -1,4 +1,5 @@
 #!/bin/bash
+gcloud auth configure-docker
 if [[ ${GIT_BRANCH} == *"master"* ]]; then                    
     gcloud docker -- push eu.gcr.io/first-k8s-project-256708/echoapp:1.0."${BUILD_NUMBER}"
 fi
